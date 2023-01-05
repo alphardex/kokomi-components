799827ccbe9b0874ee98aa69398a144cf2f10341
alphardex <2582347430@qq.com>
2023-01-05 16:14:19 +0800

    update examples

--- examples/imageTunnel/sketch.js
+++ examples/imageTunnel/sketch.js
@@ -2,6 +2,7 @@ import * as kokomi from "kokomi.js";
 import * as THREE from "three";
 import gsap from "gsap";
 import * as dat from "lil";
+import * as kokomiComponents from "kokomi-components";
 
 class Sketch extends kokomi.Base {
   async create() {
