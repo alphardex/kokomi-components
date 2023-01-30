a1cf05fa32ea8a2444c7dffe1a2176debb1278d6
alphardex <2582347430@qq.com>
2023-01-30 20:16:48 +0800

    update HologramFilter

--- src/components/hologramFilter/index.ts
+++ src/components/hologramFilter/index.ts
@@ -13,7 +13,7 @@ class HologramFilter extends POSTPROCESSING.Effect {
     progress = 1,
     glowColor = new THREE.Color("#66ccff"),
     glowColorStrength = 0.3,
-  }) {
+  } = {}) {
     super("HologramFilter", defaultFragmentShader, {
       uniforms: new Map([
         ["uProgress", new THREE.Uniform(progress)],
