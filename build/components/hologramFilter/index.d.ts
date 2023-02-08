526c00696af0646844d8332e21ad7bd7303cec37
alphardex <2582347430@qq.com>
2023-02-08 09:11:14 +0800

    remove sparkles

--- build/components/hologramFilter/index.d.ts
+++ build/components/hologramFilter/index.d.ts
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import * as POSTPROCESSING from "../../libs/postprocessing.esm.js";
 declare class HologramFilter extends POSTPROCESSING.Effect {
-    constructor({ blendFunction, progress, glowColor, glowColorStrength, }: {
+    constructor({ blendFunction, progress, glowColor, glowColorStrength, }?: {
         blendFunction?: any;
         progress?: number | undefined;
         glowColor?: THREE.Color | undefined;
--- build/components/index.d.ts
+++ build/components/index.d.ts
@@ -7,7 +7,6 @@ export * from "./liquidCrystal";
 export * from "./pencilFilter";
 export * from "./rgbShiftFilter";
 export * from "./rippleFilter";
-export * from "./sparkles";
 export * from "./sphereWordCloud";
 export * from "./starTunnel";
 export * from "./swellFilter";
